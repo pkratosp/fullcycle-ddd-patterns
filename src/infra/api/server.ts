2e59e48b78702c2fd9d508184fb09c3f37165798
Pedro <70226968+pkratosp@users.noreply.github.com>
2025-03-06 13:33:03 +0000

    instalando express e configurando api

---
+++ src/infra/api/server.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import { app } from "./express";
+
+dotenv.config();
+
+const port: number = Number(process.env.PORT) || 3333;
+
+app.listen(port, () => {
+  console.log(`Server runing ${port}...`);
+});
